76390b23735cea507dc3f2d1413b596a2ce406fa
Heno Bacalso <Heno.Bacalso@pentanasolutions.com>
2024-07-03 14:41:55 +0800

    Updated sql scripts

--- Services/SqlScripts/UserActivities.sql
+++ Services/SqlScripts/UserActivities.sql
@@ -5,7 +5,7 @@ MySQL - 5.5.5-10.1.48-MariaDB
 */
 /*!40101 SET NAMES utf8 */;
 
-create table `UserActivity` (
+create table `UserActivities` (
 	`Id` char (114),
 	`UserId` char (114),
 	`Location` varchar (450),
